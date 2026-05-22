2b88eb5305eb78c739110346c19a9ffad5f20a85
imagar2025-web <imagar2025@gmail.com>
2026-05-22 08:31:09 +0200

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,30 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    root: './backend',
+    base: '/',
+    
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            publicDirectory: '../public',
+            buildDirectory: 'dist',
         }),
         tailwindcss(),
     ],
+    
     server: {
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
+    },
+    
+    build: {
+        outDir: '../public/dist',
+        emptyOutDir: false,
+        manifest: true,
+        sourcemap: false,
+        minify: 'terser',
     },
 });
